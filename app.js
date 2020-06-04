20c8a0acd6c3aa668b89d58ff4679ae060784f7b
Debasis Jana <debasisjana93@gmail.com>
2020-06-04 11:39:01 +0530

    modifed all functions with arrow functions

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -34,7 +34,7 @@ yargs.command({
             type:'string'
         }
     },
-    handler: function (argv) {
+    handler (argv) {
         notes.removeNote(argv.title)
         }
 })
--- notes.js
+++ notes.js
@@ -1,16 +1,18 @@
 const fs=require('fs')
 const chalk = require('chalk')
-const getNotes = function () {
+
+const getNotes = ()=> 
+{
     return 'Your notes...'
 }
-const addNote= function (title, body)
+const addNote= (title, body)=>
 {
     const notes=loadNotes()
    
-    const duplicateNotes=notes.filter(function(note)
-    {
-        return note.title===title
-    })
+    const duplicateNotes=notes.filter((note)=>
+        note.title===title
+    )
+  
     if(duplicateNotes.length===0)
     {
         notes.push(
@@ -27,13 +29,13 @@ const addNote= function (title, body)
     
 }
 //Removing the note
-const removeNote =function(title)
+const removeNote =(title) =>
 {
     const notes=loadNotes()
-    const notesToKeep = notes.filter(function(notes)
-    {
-        return notes.title!=title
-    })
+    const notesToKeep = notes.filter((notes)=>
+    
+        notes.title!=title
+    )
     if(notes.length!=notesToKeep)
     {
         console.log(chalk.green.inverse("Notes removed")),
@@ -49,7 +51,7 @@ const saveNotes = function(notes)
     const dataJSON=JSON.stringify(notes)
     fs.writeFileSync('notes.json',dataJSON)
 }
-const loadNotes= function()
+const loadNotes= ()=>
 {
     try 
     {
